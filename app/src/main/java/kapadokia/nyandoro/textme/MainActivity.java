656106045a52592fce0dde6ed1801fa77b299700
Kapadokia-Titus <ktnyandoch@gmail.com>
2021-02-14 16:57:56 +0300

    working custom notification

--- app/src/main/java/kapadokia/nyandoro/textme/MainActivity.java
+++ app/src/main/java/kapadokia/nyandoro/textme/MainActivity.java
@@ -19,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String PRIMARY_CHANNEL_ID= "primary_notification_channel";
     private NotificationManager mNotificationManager;
     private static final int NOTIFICATION_ID = 0;
-    private EditText notificationText;
+    private EditText notificationText, notificationName;
+    private  String notifyName, notifyText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         notify_button = findViewById(R.id.notify);
         notificationText = findViewById(R.id.notifyText);
+        notificationName = findViewById(R.id.notifyNme);
         createNotificationChannel();
         notify_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,8 +41,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createNotificationChannel() {
+        notifyName = notificationName.getText().toString().trim();
+        notifyText = notificationText.getText().toString().trim();
 
-        String notifyRes = notificationText.getText().toString().trim();
         mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         if (android.os.Build.VERSION.SDK_INT >=
@@ -48,13 +51,13 @@ public class MainActivity extends AppCompatActivity {
             // Create a NotificationChannel
 
             NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
-                    "Kapadokia's Notification", NotificationManager
+                    notifyName, NotificationManager
                     .IMPORTANCE_HIGH);
 
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.RED);
             notificationChannel.enableVibration(true);
-            notificationChannel.setDescription(notifyRes);
+            notificationChannel.setDescription(notifyText);
             mNotificationManager.createNotificationChannel(notificationChannel);
         }
     }
@@ -66,12 +69,16 @@ public class MainActivity extends AppCompatActivity {
 
     // a helper method
     private NotificationCompat.Builder getNotificationBuilder(){
+
+        notifyName = notificationName.getText().toString().trim();
+        notifyText = notificationText.getText().toString().trim();
+
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,
                 NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
-                .setContentTitle("Notification From kapadokia")
-                .setContentText("Unajiskiaje msee")
+                .setContentTitle(notifyName)
+                .setContentText(notifyText)
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentIntent(notificationPendingIntent)
                 .setAutoCancel(true);
